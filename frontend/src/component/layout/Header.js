ff18f95b5db80fa3f0a053cf49ee0bfacb0c9f53
hiranfarias369 <hiran.farias.lima@gmail.com>
2023-01-28 16:54:48 -0300

    first commit

--- frontend/src/component/layout/Header.js
+++ frontend/src/component/layout/Header.js
@@ -1,12 +1,17 @@
 import React from 'react';
-
 import {ReactNavbar} from "overlay-navbar"
 
+
+import logo from "../../images/logo.png"
+
+
+
+const options = {
+  burgerColorHover: "#eb4034",
+  logo,}
+
 const Header = () => {
-  return <ReactNavbar />
-
-
-
-}
+  return <ReactNavbar {...options} />;
+};
 
 export default Header;
