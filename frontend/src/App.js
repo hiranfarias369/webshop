7f14cabd75bdf05b52ec1926be84cdda33974e89
hiranfarias369 <hiran.farias.lima@gmail.com>
2023-01-29 06:48:49 -0300

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,25 @@
+import "./App.css";
+import Header from "./component/layout/Header.js";
+import { BrowserRouter as Router } from "react-router-dom";
+//import webFont from "webfontloader";
+import React from "react";
 
-import './App.css';
-import Header from "./component/layout/Header.js"
-import {BrowserRouter as Router} from "react-router-dom"
 
+// React.useEffect(() => {
+//   webFont.load({
+//     google: {
+//       families: ["Robot", "Droid Sans", "Chilanka"],
+//     },
+//   });
+// }, []);
 
 function App() {
-  return <Router>
+  return (
 
-<Header />
-
-  </Router>
+    <Router>
+      <Header />
+    </Router>
+  );
 }
 
 export default App;
