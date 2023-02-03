9d99578dea4c1cc2cd8a0212a247040fb79c2d1f
hiranfarias369 <hiran.farias.lima@gmail.com>
2023-02-02 21:26:41 -0300

    first  commit

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -2,8 +2,11 @@ import axios from "axios";
 
 import {
   ALL_PRODUCT_FAIL,
-  ALL_PRODUCT_REQUESTS,
+  ALL_PRODUCT_REQUEST,
   ALL_PRODUCT_SUCCESS,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_SUCCESS,
   CLEAR_ERRORS,
 } from "../constants/productConstants";
 
@@ -12,7 +15,7 @@ import {
 export const getProduct = () => async (dispatch) => {
   try {
     dispatch({
-      type: ALL_PRODUCT_REQUESTS,
+      type: ALL_PRODUCT_REQUEST,
     });
 
     const { data } = await axios.get("/api/v1/products");
@@ -25,6 +28,31 @@ export const getProduct = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: ALL_PRODUCT_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+
+
+
+
+export const getProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: PRODUCT_DETAILS_REQUEST,
+    });
+
+    const { data } = await axios.get(`/api/v1/product/${id}`);
+
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data.product,
+    });
+
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
       payload: error.response.data.message,
     });
   }
--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -1,13 +1,18 @@
 import {
   ALL_PRODUCT_FAIL,
-  ALL_PRODUCT_REQUESTS,
+  ALL_PRODUCT_REQUEST,
   ALL_PRODUCT_SUCCESS,
+
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_SUCCESS,
+
   CLEAR_ERRORS,
 } from "../constants/productConstants";
 
 export const productsReducer = (state = { products: [] }, action) => {
   switch (action.type) {
-  case ALL_PRODUCT_REQUESTS:
+  case ALL_PRODUCT_REQUEST:
     return {
       loading: true,
       products: [],
@@ -20,7 +25,39 @@ export const productsReducer = (state = { products: [] }, action) => {
       productsCount: action.payload.productsCount,
     };
 
-  case ALL_PRODUCT_FAIL:
+  case  ALL_PRODUCT_FAIL:
+    return {
+      loading: false,
+      error: action.payload,
+    };
+
+  case CLEAR_ERRORS:
+    return {
+      ...state,
+      error: null,
+    };
+
+  default:
+    return state;
+  }
+};
+
+export const productDetailsReducer = (state = { product: {} }, action) => {
+  switch (action.type) {
+  case PRODUCT_DETAILS_REQUEST:
+    return {
+      loading: true,
+      ...state,
+    };
+
+  case PRODUCT_DETAILS_SUCCESS:
+    return {
+      loading: false,
+      product: action.payload,
+   
+    };
+
+  case PRODUCT_DETAILS_FAIL:
     return {
       loading: false,
       error: action.payload,
