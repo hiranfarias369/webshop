a982e8c97d24b9a79395556c3632c109f88c9b75
hiranfarias369 <hiran.farias.lima@gmail.com>
2023-02-16 20:06:01 -0300

    first commit

--- frontend/src/component/Cart/Cart.js
+++ frontend/src/component/Cart/Cart.js
@@ -1,13 +1,29 @@
 import React, { Fragment } from "react";
 import "./Cart.css";
 import CartItemCard from "./CartItemCard.js";
-//import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 //import { addItemsToCart, removeItemsFromCart } from "../../actions/cartAction";
 //import { Typography } from "@material-ui/core";
 //import RemoveShoppingCartIcon from "@material-ui/icons/RemoveShoppingCart";
 //import { Link } from "react-router-dom";
 
 const Cart = () => {
+  const dispatch = useDispatch();
+  const { cartItems } = useSelector((state) => state.cart);
+
+  const increaseQuantity = (id, quantity, stock)=> {
+    const newQty = quantity + 1;
+
+    if(stock <= quantity) {
+
+      return;
+
+    }
+
+dispatch(addItemsToCart(id, newQty));
+
+  };
+
   return (
     <Fragment>
       <div className="cartPage">
@@ -15,6 +31,37 @@ const Cart = () => {
           <p>Produto</p>
           <p>Quantidade</p>
           <p>Subtotal</p>
+        </div>
+
+        {cartItems &&
+          cartItems.map((item) => (
+            <div className="cartContainer">
+              <CartItemCard item={item} />
+
+              <div className="cartInput">
+                <button>-</button>
+                <input type="number" value={item.quantity} readOnly />
+                <button onClick={()=>increaseQuantity(item.product )} >+</button>
+              </div>
+              <p className="cartSubtotal">
+                {" "}
+                {`R$ ${item.price * item.quantity}`}
+              </p>
+            </div>
+          ))}
+
+        <div className="cartGrossProfit">
+          <div />
+          <div className="cartGrossProfitBox">
+            <p>Total</p>
+
+            <p>{"R$ 300"}</p>
+          </div>
+
+          <div />
+          <div className="checkOutBtn">
+            <button>Verificar</button>
+          </div>
         </div>
       </div>
     </Fragment>
--- frontend/src/component/Cart/CartItemCard.js
+++ frontend/src/component/Cart/CartItemCard.js
@@ -1,7 +1,21 @@
 import React from "react";
+import "./CartItemCard.css";
+import { Link } from "react-router-dom";
 
-const CartItemCard = () => {
-  return <div>CartItemCard</div>;
+const CartItemCard = ({ item, deleteCartItems }) => {
+  return (
+    <div className="CartItemCard">
+      <img src={item.image} alt="ssa" />
+      <div>
+        <Link to={`/product/${item.product}`}>{item.name}</Link>
+        <span>{`Preço: R$ ${item.price}`}</span>
+        
+        <p onClick={() => deleteCartItems(item.product)} aria-hidden="true">
+          Remover
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default CartItemCard;
