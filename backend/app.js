5ceec7f8fb9f313e47c460a99cf6dbf1cdf05ecd
hiranfarias369 <hiran.farias.lima@gmail.com>
2023-03-07 20:08:31 -0300

    first commit

--- backend/app.js
+++ backend/app.js
@@ -47,16 +47,10 @@ app.use("/api/v1", payment);
 
 
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-
-
-
- 
-});
-
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
 // Middleware for Errors
 app.use(errorMiddleware);
 
