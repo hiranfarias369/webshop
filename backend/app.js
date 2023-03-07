5a97c07542a79e6485cae0099bd7edc33bb9953c
hiranfarias369 <hiran.farias.lima@gmail.com>
2023-03-07 18:30:05 -0300

    first commit

--- backend/app.js
+++ backend/app.js
@@ -17,21 +17,42 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 
+
+
+
+
+
+
+
 // Route Imports
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
 const order = require("./routes/orderRoute");
 const payment = require("./routes/paymentRoute");
 
+
+
+
+
+
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
+
+
+
+
+
+
+
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  res.sendFile(path.resolve(__dirname, "frontend","build","index.html"));
+
+  // "../frontend/build/index.html"
 
  
 });
